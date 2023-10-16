87e539f29776ccd28d5f905e79574871aa68625b
R1nge <r1ngegamedev@gmail.com>
2023-10-16 14:18:27 +0300

    Death box

---
+++ Assets/Scripts/DeathBox.cs
@@ -0,0 +1,20 @@
+﻿using Players;
+using Services.States;
+using UnityEngine;
+using VContainer;
+
+public class DeathBox : MonoBehaviour
+{
+    private StateMachine _stateMachine;
+
+    [Inject]
+    private void Inject(StateMachine stateMachine) => _stateMachine = stateMachine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Player _))
+        {
+            _stateMachine.ChangeState(GameStateType.Lose);
+        }
+    }
+}
